0f12dc7c0a31aa85df23ecff1d31a5f0fe7c31e2
Chermann-KING <codes.smiling@gmail.com>
2025-03-13 16:53:38 +0100

    🗃️ Configuration de la base de données

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mssql',
+      host: 'localhost',
+      port: 1433,
+      username: 'sa',
+      password: 'YourStrong!Passw0rd',
+      database: 'garage',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      options: {
+        encrypt: false,
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
