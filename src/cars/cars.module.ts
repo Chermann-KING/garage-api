3fdf3c2ea521c203640c5407a396e22231ad8d32
Chermann-KING <codes.smiling@gmail.com>
2025-03-13 18:30:39 +0100

    🐛 Correction du module Cars par l'ajout de l'enregistrement de l'entité TypeORM
    - Ajout de l'importation de TypeOrmModule
    - Ajout de l'entité Car dans les imports avec TypeOrmModule.forFeature([Car])

--- src/cars/cars.module.ts
+++ src/cars/cars.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
-import { CarsService } from './cars.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Car } from './entities/car.entity';
 import { CarsController } from './cars.controller';
+import { CarsService } from './cars.service';
 
 @Module({
-  providers: [CarsService],
+  imports: [TypeOrmModule.forFeature([Car])],
   controllers: [CarsController],
+  providers: [CarsService],
 })
 export class CarsModule {}
