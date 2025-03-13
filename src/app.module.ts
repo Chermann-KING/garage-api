77621bfe98304d020aa6534f9635300dd1ed825d
Chermann-KING <codes.smiling@gmail.com>
2025-03-13 18:32:54 +0100

    🔧 Configuration du module App avec des variables d'environnement
    - Ajout de nestjs/config
    - Ajout de ConfigModule.forRoot() pour charger les variables d'environnement
    - Configuration de TypeORM avec les valeurs des variables d'environnement
    - Désactivation de synchronize pour éviter les problèmes de permissions

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CarsModule } from './cars/cars.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
       type: 'mssql',
-      host: 'localhost',
-      port: 1433,
-      username: 'sa',
-      password: 'YourStrong!Passw0rd',
-      database: 'garage',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || '1433', 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
       options: {
